554b9ca4904baaefd1b06610e750932e1bb8387b
VegaFromLyra <asha.ab@gmail.com>
2014-02-10 13:58:24 -0800

    Selection sort
    In each pass, compares element at index i to all the elements after it
    to find the smallest element and updates (by swapping) element[i] if a
    smaller value is found. Repeats this for each other element in the array

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = {5, 1, 3, 6, 7, 8};
+
+            SelectionSort(input);
+
+            Console.WriteLine("Sorted array is");
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i] + " ");
+            }
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            int i, j = 0;
+
+            int iMin = 0;
+
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                iMin = i;
+
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[iMin])
+                    {
+                        iMin = j;
+                    }
+                }
+
+                if (iMin != i)
+                {
+                    int temp = arr[iMin];
+                    arr[iMin] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
